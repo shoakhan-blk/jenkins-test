3e1c2d82a6919c8d0a936ec3189a11bac2a463b5
Shoaib <60384008+shoakhan-blk@users.noreply.github.com>
2020-04-24 21:46:50 +0530

    Update jenkins-timeout.groovy

--- vars/jenkins-timeout.groovy
+++ vars/jenkins-timeout.groovy
@@ -4,10 +4,10 @@ def call(String projectName) {
     echo "fetching previous build duration"
     job = hudson.model.Hudson.instance.getItem("jenkins-v2")
     build = job.getLastBuild()
-    println("Build number is ${build.getPreviousBuild().getNumber()}")
-    println("Build duration is ${build.getPreviousBuild().getDuration()}")
-    println("Build number is ${build.getPreviousBuild().getPreviousBuild().getNumber()}")
-    println("Build duration is ${build.getPreviousBuild().getPreviousBuild().getDuration()}")
+    //println("Build number is ${build.getPreviousBuild().getNumber()}")
+    //println("Build duration is ${build.getPreviousBuild().getDuration()}")
+    //println("Build number is ${build.getPreviousBuild().getPreviousBuild().getNumber()}")
+    //println("Build duration is ${build.getPreviousBuild().getPreviousBuild().getDuration()}")
     echo "fetched previous build duration" 
   } 
   catch (Exception e) {
