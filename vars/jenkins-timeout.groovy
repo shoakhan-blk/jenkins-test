fecddfd212f1bb72badb926b13244630cefe4846
Shoaib <60384008+shoakhan-blk@users.noreply.github.com>
2020-04-25 17:29:34 +0530

    Update jenkins-timeout.groovy

--- vars/jenkins-timeout.groovy
+++ vars/jenkins-timeout.groovy
@@ -19,6 +19,7 @@ def call(String projectName) {
   catch (Exception e) {
         echo "Caught exception"
    }
+  return averageElasticBuildTime
 }
 def lastSuccessfullBuildTime(previousBuild,numberOfBuilds,totalBuildTime) {
   println("calling last successful build method")
@@ -32,4 +33,3 @@ def lastSuccessfullBuildTime(previousBuild,numberOfBuilds,totalBuildTime) {
     return totalBuildTime/60
   }
 }
-return this
