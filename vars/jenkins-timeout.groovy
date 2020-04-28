efe68fcd73667a2b4d764d536ec143fcbbcc8a54
Shoaib <60384008+shoakhan-blk@users.noreply.github.com>
2020-04-28 11:35:08 +0530

    Update jenkins-timeout.groovy

--- vars/jenkins-timeout.groovy
+++ vars/jenkins-timeout.groovy
@@ -1,6 +1,6 @@
 def call(branchName,numberOfBuilds,additionalTime) {
     echo "fetching previous build duration"
-    def lastBuild=hudson.model.Hudson.instance.getItem(branchName).getLastBuild()
+    def lastBuild=hudson.model.Hudson.instance.getItem(branchName).getLastSuccessfulBuild()
     Float totalBuildTime=0.0
     Integer averageBuild=5
     return (lastSuccessfullBuildTime(lastBuild,numberOfBuilds,totalBuildTime)/averageBuild)*additionalTime
@@ -9,8 +9,9 @@ def lastSuccessfullBuildTime(previousBuild,numberOfBuilds,totalBuildTime) {
   println("calling last successful build method")
   if(previousBuild != null && numberOfBuilds>0) {
     echo "Build time of last build is ${previousBuild.getDuration()/1000.0} seconds"
+      echo "Build status of last build is ${previousBuild.getDuration()}"
     totalBuildTime=(previousBuild.getDuration()/1000.0)+totalBuildTime
-    lastSuccessfullBuildTime(previousBuild.getPreviousBuild(),--numberOfBuilds,totalBuildTime)
+    lastSuccessfullBuildTime(previousBuild.getLastSuccessfulBuild(),--numberOfBuilds,totalBuildTime)
   }
   else {
     echo "Total build time of last 5 build in minutes is ${totalBuildTime/60.0}"
