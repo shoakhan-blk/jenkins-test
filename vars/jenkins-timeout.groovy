28be8d024b06c188ef1cc0363eaca42832df97fb
Shoaib <60384008+shoakhan-blk@users.noreply.github.com>
2020-04-28 14:49:07 +0530

    updating file

--- vars/jenkins-timeout.groovy
+++ vars/jenkins-timeout.groovy
@@ -1,6 +1,38 @@
 #!/usr/bin/env groovy
 import hudson.model.Hudson
 
+/**
+ * @param branch_name name of the branch
+ * @param number_of_builds number of builds to find the elastic time
+ * @param additional_time additional time that gets added to the average build time
+ * @return return the elastic build time in minutes
+ */
+def call(branch_name,number_of_builds,additional_time) {
+    def last_successful_build=Hudson.instance.getItem(branch_name).getLastSuccessfulBuild()
+    return (sumOfLastSuccessfulBuildTime(last_successful_build,number_of_builds,0.0)/number_of_builds)*additional_time
+}
+/**
+ * Recusively calculate the sum of build duration
+ * @param last_successful_build last successful build 
+ * @param number_of_builds number of builds to find the elastic time
+ * @param total_build_time additional time that gets added to the average build time
+ * @return return the total build time in minutes
+ */
+def sumOfLastSuccessfulBuildTime(last_successful_build,number_of_builds,total_build_time) {
+  if(last_successful_build != null && number_of_builds>0) {
+    total_build_time=(last_successful_build.getDuration()/1000.0)+total_build_time
+    sumOfLastSuccessfulBuildTime(last_successful_build.getPreviousSuccessfulBuild(),--number_of_builds,total_build_time)
+  }
+  else {
+    return total_build_time/60.0
+  }
+}
+return this
+
+/**
+#!/usr/bin/env groovy
+import hudson.model.Hudson
+
 def call(branchName,numberOfBuilds,additionalTime) {
     echo "fetching previous build duration"
     def lastBuild=Hudson.instance.getItem(branchName).getLastSuccessfulBuild()
@@ -20,3 +52,4 @@ def totalLastSuccessfulBuildTime(previousBuild,numberOfBuilds,totalBuildTime) {
   }
 }
 return this
+ */
