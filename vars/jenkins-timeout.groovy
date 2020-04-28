699650b3b3ea31957be635a433e66fab46c0d22e
Shoaib <60384008+shoakhan-blk@users.noreply.github.com>
2020-04-28 12:05:37 +0530

    Update jenkins-timeout.groovy

--- vars/jenkins-timeout.groovy
+++ vars/jenkins-timeout.groovy
@@ -2,6 +2,7 @@ def call(branchName,numberOfBuilds,additionalTime) {
     echo "fetching previous build duration"
     def lastBuild=hudson.model.Hudson.instance.getItem(branchName).getLastSuccessfulBuild()
     println("type of class is ${lastBuild.getClass()}")
+    println("type of class after typecasting is ${(hudson.model.Hudson.instance)lastBuild.getClass()}")
     Float totalBuildTime=0.0
     Integer averageBuild=5
     return (lastSuccessfullBuildTime(lastBuild,numberOfBuilds,totalBuildTime)/averageBuild)*additionalTime
